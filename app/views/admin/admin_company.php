68c20786e099b7c0b830b7e25efc4e86cd729856
Maikel <maikel-93@hotmail.com>
2019-02-08 14:25:33 -0200

    iniciar company

--- app/views/admin/admin_company.php
+++ app/views/admin/admin_company.php
@@ -12,153 +12,130 @@ $this->load->view("admin/admin_header");
     $this->load->view("admin/admin_menu");
     $this->load->view("admin/admin_menu_header");
     ?>
-    
-
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <section class="content-header">
-        <h1>
+            <h1>
             Empresa
-        </h1>
+            </h1>
         </section>
         <!-- Main content -->
         <section class="content">
-        <!-- Small boxes (Stat box) -->
-        <div class="row">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
             <!-- left column -->
             <div class="col-md-12">
-            <!-- general form elements -->
-            <div class="box box-primary">
+                <!-- general form elements -->
+                <div class="box box-primary">
                 <?php
-                $this->load->view("admin/admin_header");
+                $this->load->view("admin/admin_company_navigation");
                 ?>
                 <!-- /.box-header -->
-                <?=form_open("admin/user/post");?>
                 <div class="box-body">
+                <?php
+                if ($this->session->flashdata('success')) {
+                    ?>
+                    <div class="alert alert-success alert-dismissible">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                        <h4><i class="icon fa  fa-check-circle-o"></i> Concluído</h4>
+                        <?=$this->session->flashdata('success');?>
+                    </div>
                     <?php
-                    if (validation_errors()) {
-                        ?>
-                        <div class="alert alert-danger alert-dismissible">
-                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                            <h4><i class="icon fa fa-ban"></i> Erro</h4>
-                            <?=validation_errors();?>
-                        </div>
+                }
+                ?>
+                <?php
+                if ($this->session->flashdata('error')) {
+                    ?>
+                    <div class="alert alert-danger alert-dismissible">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                        <h4><i class="icon fa fa-ban"></i> Erro</h4>
+                        <?=$this->session->flashdata('error');?>
+                    </div>
+                    <?php
+                }
+                ?>
+                <div class="col-md-12">
+                    <!-- datatable-start -->
+                    <table id="user-table" class="table table-bordered table-hover">
+                        <thead>
+                        <tr>
+                        <th>Nome</th>
+                        <th>Usuário</th>
+                        <th>E-mail</th>
+                        <th>Ação</th>
+                        </tr>
+                        </thead>
+                        <tbody>
                         <?php
-                    }
-                    ?>
-                    <div class="form-group">
-                        <?=form_label("Primeiro nome: (Obrigatório)", "first_name");?>
-                        <?=form_input(
-                            array(
-                                "id" => "first_name",
-                                "name" => "first_name",
-                                "type" => "text",
-                                "class" => "form-control",
-                                "placeholder" => "Primeiro nome",
-                                "required" => "true",
-                                "autocomplete" => "off",
-                                "value" => set_value("first_name")
-                            )
-                        );?>
-                    </div>
-                    <div class="form-group">
-                        <?=form_label("Segundo nome:", "middle_name");?>
-                        <?=form_input(
-                            array(
-                                "id" => "middle_name",
-                                "name" => "middle_name",
-                                "type" => "text",
-                                "class" => "form-control",
-                                "placeholder" => "Segundo nome",
-                                "required" => "false",
-                                "autocomplete" => "off",
-                                "value" => set_value("middle_name")
-                            )
-                        );?>
-                    </div>
-                    <div class="form-group">
-                        <?=form_label("Último nome: (Obrigatório)", "last_name");?>
-                        <?=form_input(
-                            array(
-                                "id" => "last_name",
-                                "name" => "last_name",
-                                "type" => "text",
-                                "class" => "form-control",
-                                "placeholder" => "Último nome",
-                                "required" => "true",
-                                "autocomplete" => "off",
-                                "value" => set_value("last_name")
-                            )
-                        );?>
-                    </div>
-                    <div class="form-group">
-                        <?=form_label("Nome de usuário: (Obrigatório, mínimo 6 caracteres)", "username");?>
-                        <?=form_input(
-                            array(
-                                "id" => "username",
-                                "name" => "username",
-                                "type" => "text",
-                                "class" => "form-control",
-                                "placeholder" => "Nome de usuário",
-                                "required" => "true",
-                                "autocomplete" => "off",
-                                "value" => set_value("username")
-                            )
-                        );?>
-                    </div>
-                    <div class="form-group">
-                        <?=form_label("E-mail: (Obrigatório)", "email");?>
-                        <?=form_input(
-                            array(
-                                "id" => "email",
-                                "name" => "email",
-                                "type" => "email",
-                                "class" => "form-control",
-                                "placeholder" => "E-mail",
-                                "required" => "true",
-                                "autocomplete" => "off",
-                                "value" => set_value("email")
-                            )
-                        );?>
-                    </div>
-                    <div class="form-group">
-                        <?=form_label("Senha: (Obrigatório)", "password");?>
-                        <?=form_password(
-                            array(
-                                "id" => "password",
-                                "name" => "password",
-                                "class" => "form-control",
-                                "placeholder" => "Senha"
-                            )
-                        );?>
-                    </div>
-                    <div class="form-group">
-                        <?=form_label("Confirme a senha: (Obrigatório)", "password");?>
-                        <?=form_password(
-                            array(
-                                "id" => "password_conf",
-                                "name" => "password_conf",
-                                "class" => "form-control",
-                                "placeholder" => "Senha"
-                            )
-                        );?>
-                    </div>
-                    <?=form_submit(
-                        array(
-                            "value" => "Gravar",
-                            "type" => "submit",
-                            "name" => "post",
-                            "class" => "btn btn-primary btn-block btn-flat",
-                        )
-                    );
-                    ?>
+                        if (!empty($array_user)) {
+                            foreach ($array_user as $user) {
+                                ?>
+                                <tr>
+                                <td>
+                                    <?=$user->complete_name;?>
+                                </td>
+                                <td>
+                                    <?=$user->username;?>
+                                </td>
+                                <td>
+                                    <?=$user->email;?>
+                                </td>
+                                <td>
+                                    <a href="<?=site_url("admin/user/put/".$user->id);?>"
+                                    class="btn btn-block btn-info btn-flat">Editar</button>
+                                    <a href="" class="btn btn-block btn-danger btn-flat" data-toggle="modal"
+                                    data-target="#modal-danger" onclick="trocaId('<?=$user->id;?>')">Excluir</a>
+                                </td>
+                                </tr>
+                                <?php
+                            }
+                        }
+                        ?>
+                    </table>
                 </div>
-                <?=form_close();?>
-            </div>
+                </div>
+                </div>
             </div>
         </div>
     </aside>
+    <div class="modal modal-danger fade" id="modal-danger" style="display: none;">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span></button>
+                <h4 class="modal-title">Confirme a operação</h4>
+                </div>
+                <?=form_open("admin/user/delete");?>
+                <?=form_input(
+                    array(
+                        "type" => "hidden",
+                        "name" => "id",
+                        "id" => "user-id"
+                    )
+                );?>
+                <div class="modal-body">
+                <p>Excluir usuário</p>
+                </div>
+                <div class="modal-footer">
+                <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Cancelar</button>
+                <?=form_submit(
+                    array(
+                        "value" => "Confirmar",
+                        "type" => "submit",
+                        "name" => "post",
+                        "class" => "btn btn-outline",
+                    )
+                );
+                ?>
+                <?=form_close();?>
+                </div>
+            </div>
+        <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
     <!-- /.control-sidebar -->
     <!-- Add the sidebar's background. This div must be placed
         immediately after the control sidebar -->
@@ -168,5 +145,21 @@ $this->load->view("admin/admin_header");
 <?php
 $this->load->view("admin/admin_footer");
 ?>
+<script>
+    $(function () {
+        $('#user-table').DataTable({
+        'paging'      : true,
+        'lengthChange': false,
+        'searching'   : false,
+        'ordering'    : false,
+        'info'        : true,
+        'autoWidth'   : false
+        })
+    })
+
+    function trocaId(id) {
+        $("#user-id").val(id);
+    }
+</script>
 </body>
 </html>
